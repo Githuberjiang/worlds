12e6d6df12b2f2096e288971e09b16fce702584c
Amery_J <820293142@qq.com>
2019-12-25 14:30:26 +0800

    basic vocabulary

---
+++ basic vocabulary/111.py
@@ -0,0 +1,22 @@
+# a = 0
+# with open("词性", "r", encoding="utf-8") as f:
+#     for i in f.readlines():
+#
+#         i = str(a) + " " + i.strip()
+#         if i == "1 词性缩写":
+#             pass
+#         else:
+#             with open(i, "w", encoding="utf-8") as ff:
+#                 pass
+#         a += 1
+
+
+# a = 0
+# with open("unite", "r", encoding="utf-8") as f:
+#     for i in f.readlines():
+#         a += 1
+#         i = i.strip()
+#         with open(i, "w", encoding="utf-8") as ff:
+#             pass
+
+
---
+++ basic vocabulary/unite1.md
@@ -0,0 +1,9 @@
+able a.
+ability n.
+enable vt.
+disable vt.
+disability n.
+about ad. prep. a.
+beside prep.
+besides prep.
+near prep.
